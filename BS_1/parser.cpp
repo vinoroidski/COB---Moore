0a613adc9202c57d046f59c5bdc00ffd10add3ac
Diego M. Rio <dmrio68@gmail.com>
2014-04-25 19:27:45 +0200

    Added "add", "sub", "mul" and "div" methods

--- BS_1/parser.cpp
+++ BS_1/parser.cpp
@@ -16,3 +16,18 @@ parser::parser(const parser& orig) {
 parser::~parser() {
 }
 
+int parser::add(int x, int y){
+    return x + y;
+}
+
+int parser::sub(int x, int y){
+    return x - y;
+}
+
+int parser::mul(int x, int y){
+    return x * y;
+}
+
+int parser::div(int x, int y){
+    return x / y;
+}
