70e44cc07de69754de0549e62231d76e953247b1
Diego M. Rio <dmrio68@gmail.com>
2014-04-25 19:28:57 +0200

    Added "add", "sub", "mul" and "div" methods

--- BS_1/parser.h
+++ BS_1/parser.h
@@ -19,6 +19,10 @@ public:
     virtual ~parser();
     
     void read();
+    int add (int x, int y);
+    int sub (int x, int y);
+    int mul (int x, int y);
+    int div (int x, int y);
     
 private:
     vector<char> input;
